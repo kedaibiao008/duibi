ba11026d3df2cbcd656afaa74a93b389e32bbceb
kedaibiao008 <118370965+kedaibiao008@users.noreply.github.com>
2023-10-28 23:13:31 +0800

    Update duibi.py

--- duibi.py
+++ duibi.py
@@ -1,37 +1,25 @@
 import time
 import os
+import re
 
 def read_file_lines(file_path):
     with open(file_path, "r") as file:
-        return set(line.strip() for line in file)
+        return file.read()
 
-def save_data_to_html(data, output_path):
-    with open(output_path, "w") as file:
-        file.write("\n".join(data))
-
-def process_data(initial_realmbase, abc_path, bcd_path):
-    # 读取初始 realmbase.txt 和 abc.txt 的数据
-    realmbase_data = read_file_lines(initial_realmbase)
-    abc_data = read_file_lines(abc_path)
-
-    # 找出仅存在于 abc.txt 中的数据
-    new_data = abc_data.difference(realmbase_data)
-
-    # 保存新增数据到 realm1.html
-    save_data_to_html(new_data, bcd_path)
+def extract_three_letter_combinations(text):
+    # 使用正则表达式提取3位组合（数字和字母）
+    return re.findall(r'\b\w{3}\b', text)
 
 def main():
     realmbase_path = os.path.expanduser("~/atom/ok/realmbase.txt")
-    abc_path = os.path.expanduser("~/atom/ok/abc.txt")
-    bcd_path = os.path.expanduser("~/var/www/html/realm1.html")
     
-    # 初始化时处理所有数据
-    process_data(realmbase_path, abc_path, bcd_path)
-
-    while True:
-        # 处理新增数据每隔 10 秒
-        process_data(realmbase_path, abc_path, bcd_path)
-        time.sleep(10)
+    # 读取 realmbase.txt 的数据
+    realmbase_data = read_file_lines(realmbase_path)
+    
+    # 提取3位组合（任意数字和字母）并打印
+    combinations = extract_three_letter_combinations(realmbase_data)
+    for combination in combinations:
+        print(combination)
 
 if __name__ == "__main__":
     main()
